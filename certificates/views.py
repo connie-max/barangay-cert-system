2f2162858b27c2f729da68f86900420c22588836
connie-max <conconmanguiran@gmail.com>
2026-09-03 15:51:10 +0800

    Add certificate request form and my requests page

--- certificates/views.py
+++ certificates/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .forms import CertificateRequestForm
+from .models import Resident, CertificateRequest
 
 @login_required
 def home(request):
     return render(request, 'certificates/home.html')
+
+@login_required
+def request_certificate(request):
+    resident = Resident.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        form = CertificateRequestForm(request.POST)
+        if form.is_valid():
+            new_request = form.save(commit=False)
+            new_request.resident = resident
+            new_request.save()
+            return redirect('my_requests')
+    else:
+        form = CertificateRequestForm()
+
+    return render(request, 'certificates/request_form.html', {'form': form})
+
+@login_required
+def my_requests(request):
+    resident = Resident.objects.get(user=request.user)
+    requests = CertificateRequest.objects.filter(resident=resident)
+    return render(request, 'certificates/my_requests.html', {'requests': requests})
--- config/urls.py
+++ config/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='certificates/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('', certificates_views.home, name='home'),
+    path('request/', certificates_views.request_certificate, name='request_certificate'),
+    path('my-requests/', certificates_views.my_requests, name='my_requests'),
 ]
